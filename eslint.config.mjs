6672e4c0dddd795979311c55d555bab137582dca
Hebheb <hebheb@fim.uni-passau.de>
2026-03-26 23:15:32 +0100

    first commit

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,30 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
-import nextTs from "eslint-config-next/typescript";
+import nextConfig from 'eslint-config-next';
+import prettierConfig from 'eslint-config-prettier';
 
-const eslintConfig = defineConfig([
-  ...nextVitals,
-  ...nextTs,
-  // Override default ignores of eslint-config-next.
-  globalIgnores([
-    // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
-  ]),
-]);
+const eslintConfig = [
+  {
+    ignores: [
+      '**/._*',
+      '**/.DS_Store',
+      '.next/**',
+      'out/**',
+      'dist/**',
+      'build/**',
+      'node_modules/**',
+      '.env*',
+      'supabase/.temp/**',
+      'supabase/.branches/**',
+    ],
+  },
+  ...nextConfig,
+  prettierConfig,
+  {
+    rules: {
+      semi: ['error', 'always'],
+      'prefer-arrow-callback': 'error',
+      'prefer-template': 'off',
+    },
+  },
+];
 
 export default eslintConfig;
